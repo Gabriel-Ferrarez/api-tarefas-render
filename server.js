a0c5a8c0e5ebdd8a032673ae26fd369b63c2f4e9
Gabriel Oliveira <gabrielferrarez77@gmai.com>
2026-05-29 20:27:48 -0300

    feat(api): atualizar versao para 1.0.3

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ let tarefas = [
 app.get("/", (req, res) => {
   res.json({
     status: "API de Tarefas rodando com CI/CD no Render",
-    versao: "1.0.2",
+    versao: "1.0.3",
     timestamp: new Date().toISOString()
   });
 });
